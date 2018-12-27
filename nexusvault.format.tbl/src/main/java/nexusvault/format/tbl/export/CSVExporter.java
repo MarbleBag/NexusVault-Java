2c102412b709ebad97c0d58b21960ff072685854
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2018-12-27 17:58:49 +0100

    Exporter: tbl -> csv

---
+++ nexusvault.format.tbl/src/main/java/nexusvault/format/tbl/export/CSVExporter.java
@@ -0,0 +1,45 @@
+package nexusvault.format.tbl.export;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+import nexusvault.format.tbl.RawTable;
+import nexusvault.format.tbl.TableRecord;
+
+public class CSVExporter {
+
+	private final String elementDelimiter;
+
+	public CSVExporter() {
+		this(";");
+	}
+
+	public CSVExporter(String elementDelimiter) {
+		this.elementDelimiter = elementDelimiter;
+	}
+
+	public void export(RawTable table, Writer out) throws IOException {
+		final int fieldCount = table.getFieldCount();
+		for (int i = 0; i < fieldCount; ++i) {
+			if (i != 0) {
+				out.append(elementDelimiter);
+			}
+			out.append(table.getFieldName(i));
+		}
+		out.append("\n");
+
+		final List<TableRecord> records = table.getRecords();
+		for (final TableRecord record : records) {
+			for (int j = 0; j < fieldCount; ++j) {
+				if (j != 0) {
+					out.append(elementDelimiter);
+				}
+				final Object data = record.get(j);
+				out.append(data != null ? data.toString() : "null");
+			}
+			out.append("\n");
+		}
+	}
+
+}
