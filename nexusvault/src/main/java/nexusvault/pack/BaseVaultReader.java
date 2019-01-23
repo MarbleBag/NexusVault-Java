b0d991fa86eccbea4cfe68874d39fbbe5ee44ab9
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2019-01-23 15:26:04 +0100

    remake VaultReader as interface

--- nexusvault/src/main/java/nexusvault/pack/BaseVaultReader.java
+++ nexusvault/src/main/java/nexusvault/pack/BaseVaultReader.java
@@ -3,70 +3,113 @@ package nexusvault.pack;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.EnumSet;
 
 import kreed.io.util.BinaryReader;
-import kreed.io.util.Seek;
+import kreed.io.util.SeekableByteChannelBinaryReader;
+import nexusvault.pack.archive.AARCEntry;
+import nexusvault.pack.archive.ArchiveFile;
+import nexusvault.pack.archive.ArchiveFileReader;
 import nexusvault.pack.index.IdxDirectory;
 import nexusvault.pack.index.IdxFileLink;
-import nexusvault.shared.exception.IntegerOverflowException;
+import nexusvault.pack.index.IndexFile;
+import nexusvault.pack.index.IndexFileReader;
 
-abstract class BaseVaultReader {
+final class BaseVaultReader extends AbstVaultReader {
 
-	private final VaultUnpacker UNPACK_ZIP = new ZipInflaterVaultUnpacker();
-	private final VaultUnpacker UNPACK_LZMA = new SevenZipLZMAVaultUnpacker();
+	private IndexFile fileIndex;
+	private ArchiveFile fileArchive;
+	private Path pathArchive;
+	private FileAccessCache archiveCache;
 
 	public BaseVaultReader() {
 
 	}
 
-	abstract void startArchiveAccess() throws IOException;
+	@Override
+	public void readArchive(Path path) throws IOException {
+		dispose();
 
-	abstract void endArchiveAccess();
+		String fileName = path.getFileName().toString();
+		Path idxPath = null;
+		Path arcPath = null;
 
-	abstract BinaryReader getArchiveBinaryReader() throws IOException;
-
-	abstract PackHeader getPackHeaderForFile(IdxFileLink file);
-
-	abstract void dispose();
-
-	abstract IdxDirectory getRootFolder();
-
-	final public ByteBuffer getData(IdxFileLink file) throws IOException {
-		final PackHeader block = getPackHeaderForFile(file);
-
-		startArchiveAccess();
-
-		final BinaryReader reader = getArchiveBinaryReader();
-		try {
-
-			reader.seek(Seek.BEGIN, block.getOffset());
-			final int compressionType = file.getFlags();
-
-			ByteBuffer result = null;
-			switch (compressionType) {
-				case 3: // zip
-					result = UNPACK_ZIP.unpack(reader, file.getCompressedSize(), file.getUncompressedSize());
-					break;
-
-				case 5: // lzma
-					result = UNPACK_LZMA.unpack(reader, file.getCompressedSize(), file.getUncompressedSize());
-					break;
-
-				default: // uncompressed
-					final long dataSize = block.getSize();
-					if ((dataSize < 0) || (dataSize > Integer.MAX_VALUE)) {
-						throw new IntegerOverflowException();
-					}
-					final byte[] buffer = new byte[(int) block.getSize()];
-					reader.readInt8(buffer, 0, buffer.length);
-					result = ByteBuffer.wrap(buffer);
-			}
-
-			result.order(ByteOrder.LITTLE_ENDIAN);
-			return result;
-		} finally {
-			endArchiveAccess();
+		if (fileName.endsWith(".index")) {
+			idxPath = path;
+			fileName = fileName.substring(0, fileName.lastIndexOf(".index")) + ".archive";
+			arcPath = path.resolveSibling(fileName);
+		} else if (fileName.endsWith(".archive")) {
+			arcPath = path;
+			fileName = fileName.substring(0, fileName.lastIndexOf(".archive")) + ".index";
+			idxPath = path.resolveSibling(fileName);
+		} else {
+			throw new IllegalArgumentException("todo");
 		}
+
+		this.pathArchive = arcPath;
+
+		try (SeekableByteChannel fileStream = Files.newByteChannel(idxPath, EnumSet.of(StandardOpenOption.READ))) {
+			final BinaryReader fileReader = new SeekableByteChannelBinaryReader(fileStream, ByteBuffer.allocate(32 * 1024).order(ByteOrder.LITTLE_ENDIAN));
+			this.fileIndex = new IndexFileReader().read(fileReader);
+		}
+
+		try (SeekableByteChannel fileStream = Files.newByteChannel(arcPath, EnumSet.of(StandardOpenOption.READ))) {
+			final BinaryReader fileReader = new SeekableByteChannelBinaryReader(fileStream, ByteBuffer.allocate(32 * 1024).order(ByteOrder.LITTLE_ENDIAN));
+			this.fileArchive = new ArchiveFileReader().read(fileReader);
+		}
+
+		this.archiveCache = new FileAccessCache(60000, this.pathArchive, 4 * 1024 * 1024, ByteOrder.LITTLE_ENDIAN);
+	}
+
+	@Override
+	public void dispose() {
+		if (this.archiveCache == null) {
+			return;
+		}
+
+		this.fileIndex = null;
+		this.fileArchive = null;
+		this.pathArchive = null;
+
+		this.archiveCache.shutDown();
+		this.archiveCache = null;
+	}
+
+	@Override
+	public boolean isDisposed() {
+		return this.archiveCache == null;
+	}
+
+	@Override
+	public IdxDirectory getRootFolder() {
+		return fileIndex.getRootDirectory();
+	}
+
+	@Override
+	void startArchiveAccess() {
+
+	}
+
+	@Override
+	void endArchiveAccess() {
+		archiveCache.startExpiring();
+	}
+
+	@Override
+	BinaryReader getArchiveBinaryReader() throws IOException {
+		final BinaryReader reader = archiveCache.getChannel();
+		return reader;
+	}
+
+	@Override
+	PackHeader getPackHeaderForFile(IdxFileLink file) {
+		final AARCEntry entry = fileArchive.getEntry(file.getShaHash());
+		final PackHeader header = fileArchive.getPackHeader(entry);
+		return header;
 	}
 
 }
--- nexusvault/src/main/java/nexusvault/pack/VaultReader.java
+++ nexusvault/src/main/java/nexusvault/pack/VaultReader.java
@@ -2,106 +2,24 @@ package nexusvault.pack;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.channels.SeekableByteChannel;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.EnumSet;
 
-import kreed.io.util.BinaryReader;
-import kreed.io.util.SeekableByteChannelBinaryReader;
-import nexusvault.pack.archive.AARCEntry;
-import nexusvault.pack.archive.ArchiveFile;
-import nexusvault.pack.archive.ArchiveFileReader;
 import nexusvault.pack.index.IdxDirectory;
 import nexusvault.pack.index.IdxFileLink;
-import nexusvault.pack.index.IndexFile;
-import nexusvault.pack.index.IndexFileReader;
 
-public final class VaultReader extends BaseVaultReader {
+public interface VaultReader {
 
-	private IndexFile fileIndex;
-	private ArchiveFile fileArchive;
-	private Path pathArchive;
-	private FileAccessCache archiveCache;
-
-	public VaultReader() {
-
+	public static VaultReader createVaultReader() {
+		return new BaseVaultReader();
 	}
 
-	public void readArchive(Path path) throws IOException {
-		String fileName = path.getFileName().toString();
-		Path idxPath = null;
-		Path arcPath = null;
+	void readArchive(Path path) throws IOException;
 
-		if (fileName.endsWith(".index")) {
-			idxPath = path;
-			fileName = fileName.substring(0, fileName.lastIndexOf(".index")) + ".archive";
-			arcPath = path.resolveSibling(fileName);
-		} else if (fileName.endsWith(".archive")) {
-			arcPath = path;
-			fileName = fileName.substring(0, fileName.lastIndexOf(".archive")) + ".index";
-			idxPath = path.resolveSibling(fileName);
-		} else {
-			throw new IllegalArgumentException("todo");
-		}
+	ByteBuffer getData(IdxFileLink file) throws IOException;
 
-		this.pathArchive = arcPath;
+	IdxDirectory getRootFolder();
 
-		try (SeekableByteChannel fileStream = Files.newByteChannel(idxPath, EnumSet.of(StandardOpenOption.READ))) {
-			final BinaryReader fileReader = new SeekableByteChannelBinaryReader(fileStream, ByteBuffer.allocate(32 * 1024).order(ByteOrder.LITTLE_ENDIAN));
-			this.fileIndex = new IndexFileReader().read(fileReader);
-		}
+	void dispose();
 
-		try (SeekableByteChannel fileStream = Files.newByteChannel(arcPath, EnumSet.of(StandardOpenOption.READ))) {
-			final BinaryReader fileReader = new SeekableByteChannelBinaryReader(fileStream, ByteBuffer.allocate(32 * 1024).order(ByteOrder.LITTLE_ENDIAN));
-			this.fileArchive = new ArchiveFileReader().read(fileReader);
-		}
-
-		this.archiveCache = new FileAccessCache(60000, this.pathArchive, 4 * 1024 * 1024, ByteOrder.LITTLE_ENDIAN);
-	}
-
-	@Override
-	public void dispose() {
-		if (this.archiveCache == null) {
-			return;
-		}
-
-		this.fileIndex = null;
-		this.fileArchive = null;
-		this.pathArchive = null;
-
-		this.archiveCache.shutDown();
-		return;
-	}
-
-	@Override
-	public IdxDirectory getRootFolder() {
-		return fileIndex.getRootDirectory();
-	}
-
-	@Override
-	void startArchiveAccess() {
-
-	}
-
-	@Override
-	void endArchiveAccess() {
-		archiveCache.startExpiring();
-	}
-
-	@Override
-	BinaryReader getArchiveBinaryReader() throws IOException {
-		final BinaryReader reader = archiveCache.getChannel();
-		return reader;
-	}
-
-	@Override
-	PackHeader getPackHeaderForFile(IdxFileLink file) {
-		final AARCEntry entry = fileArchive.getEntry(file.getShaHash());
-		final PackHeader header = fileArchive.getPackHeader(entry);
-		return header;
-	}
-
+	boolean isDisposed();
 }
