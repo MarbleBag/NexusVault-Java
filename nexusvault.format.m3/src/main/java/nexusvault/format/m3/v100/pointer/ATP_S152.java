36434e790050f8d8dee4180bf263c7270471e881
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2019-02-06 14:32:31 +0100

    Add layout for an unknown struct '152' to m3

--- nexusvault.format.m3/src/main/java/nexusvault/format/m3/v100/pointer/ATP_S152.java
+++ nexusvault.format.m3/src/main/java/nexusvault/format/m3/v100/pointer/ATP_S152.java
@@ -1,7 +1,9 @@
 package nexusvault.format.m3.v100.pointer;
 
-public class ATP_S152 extends ArrayTypePointer<NoType> {
+import nexusvault.format.m3.v100.struct.StructUnk152;
+
+public final class ATP_S152 extends ArrayTypePointer<StructUnk152> {
 	public ATP_S152() {
-		super(152);
+		super(StructUnk152.class);
 	}
 }
