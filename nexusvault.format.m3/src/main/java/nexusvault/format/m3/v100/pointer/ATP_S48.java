79c1809b348f99736d7883dd66c166ebdfe581e1
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2019-02-07 20:52:38 +0100

    Add m3 header struct S48

--- nexusvault.format.m3/src/main/java/nexusvault/format/m3/v100/pointer/ATP_S48.java
+++ nexusvault.format.m3/src/main/java/nexusvault/format/m3/v100/pointer/ATP_S48.java
@@ -1,7 +1,9 @@
 package nexusvault.format.m3.v100.pointer;
 
-public class ATP_S48 extends ArrayTypePointer<NoType> {
+import nexusvault.format.m3.v100.struct.StructUnk48;
+
+public final class ATP_S48 extends ArrayTypePointer<StructUnk48> {
 	public ATP_S48() {
-		super(48);
+		super(StructUnk48.class);
 	}
 }
