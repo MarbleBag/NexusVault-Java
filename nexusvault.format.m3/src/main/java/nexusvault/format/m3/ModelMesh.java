ff409e70fd5c48c68dde0eb49e077e61c6dd8428
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2019-05-29 12:30:25 +0200

    Update Javadoc on ModelMesh

--- nexusvault.format.m3/src/main/java/nexusvault/format/m3/ModelMesh.java
+++ nexusvault.format.m3/src/main/java/nexusvault/format/m3/ModelMesh.java
@@ -11,15 +11,25 @@ public interface ModelMesh {
 
 	int getMaterialReference();
 
+	/**
+	 * @return true if this mesh belongs to a group
+	 * @see #getMeshGroup()
+	 */
 	boolean hasMeshGroup();
 
+	/**
+	 * Returns the group id of this mesh. A group is optional. Check {@link #hasMeshGroup()} before obtaining this value.
+	 *
+	 * @return the group id
+	 * @see #hasMeshGroup()
+	 */
 	int getMeshGroup();
 
 	/**
 	 * A mesh can be assigned to a specific part of a body. The meaning of some of its values can be looked up in {@link MeshToBodyPart}
 	 * <p>
 	 * It seems this value is only used in models of playable races.
-	 * 
+	 *
 	 * @return the index of assigned body part
 	 * @see MeshToBodyPart
 	 */
