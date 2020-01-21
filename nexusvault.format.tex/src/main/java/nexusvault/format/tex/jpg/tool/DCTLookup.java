3bc8d142ba69c6486c2160cc26dc4815b6eb66ba
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2020-01-22 00:30:09 +0100

    fix formatting

--- nexusvault.format.tex/src/main/java/nexusvault/format/tex/jpg/tool/DCTLookup.java
+++ nexusvault.format.tex/src/main/java/nexusvault/format/tex/jpg/tool/DCTLookup.java
@@ -1,6 +1,6 @@
 package nexusvault.format.tex.jpg.tool;
 
-final class DCTLookup {
+public final class DCTLookup {
 
 	private static final double Cx_sqr = 1 / Math.sqrt(2);
 
@@ -8,17 +8,17 @@ final class DCTLookup {
 	private final int max;
 
 	public DCTLookup(int rows, int columns) {
-		max = Math.max(rows, columns);
-		lookup = new double[max * max];
-		for (int x0 = 0; x0 < max; x0++) {
-			for (int x = 0; x < max; x++) {
-				lookup[x + (x0 * max)] = Math.cos((((2f * x0) + 1f) * x * Math.PI) / 16f);
+		this.max = Math.max(rows, columns);
+		this.lookup = new double[this.max * this.max];
+		for (int x0 = 0; x0 < this.max; x0++) {
+			for (int x = 0; x < this.max; x++) {
+				this.lookup[x + x0 * this.max] = Math.cos((2f * x0 + 1f) * x * Math.PI / 16f);
 			}
 		}
 	}
 
 	public double cof(int x, int x0) {
-		return lookup[x + (x0 * max)];
+		return this.lookup[x + x0 * this.max];
 	}
 
 	public double C(int n) {
@@ -26,54 +26,54 @@ final class DCTLookup {
 	}
 
 	public double DCT(int x0, int y0, int[] matrix, int offset, int rows, int columns) {
-		if (max < Math.max(rows, columns)) {
+		if (this.max < Math.max(rows, columns)) {
 			throw new IndexOutOfBoundsException();
 		}
-		if ((x0 < 0) || (y0 < 0) || (max < x0) || (max < y0) || (rows <= 0) || (columns <= 0)) {
+		if (x0 < 0 || y0 < 0 || this.max < x0 || this.max < y0 || rows <= 0 || columns <= 0) {
 			throw new IndexOutOfBoundsException();
 		}
 
 		double accu = 0;
 		for (int y = 0; y < rows; ++y) {
 			for (int x = 0; x < columns; ++x) {
-				accu += cof(x, x0) * cof(y, y0) * matrix[offset + x + (y * columns)];
+				accu += cof(x, x0) * cof(y, y0) * matrix[offset + x + y * columns];
 			}
 		}
-		return (1f / 4f) * C(x0) * C(y0) * accu;
+		return 1f / 4f * C(x0) * C(y0) * accu;
 	}
 
 	public double DCT(int x0, int y0, short[] matrix, int offset, int rows, int columns) {
-		if (max < Math.max(rows, columns)) {
+		if (this.max < Math.max(rows, columns)) {
 			throw new IndexOutOfBoundsException();
 		}
-		if ((x0 < 0) || (y0 < 0) || (max < x0) || (max < y0) || (rows <= 0) || (columns <= 0)) {
+		if (x0 < 0 || y0 < 0 || this.max < x0 || this.max < y0 || rows <= 0 || columns <= 0) {
 			throw new IndexOutOfBoundsException();
 		}
 
 		double accu = 0;
 		for (int y = 0; y < rows; ++y) {
 			for (int x = 0; x < columns; ++x) {
-				accu += cof(x, x0) * cof(y, y0) * matrix[offset + x + (y * columns)];
+				accu += cof(x, x0) * cof(y, y0) * matrix[offset + x + y * columns];
 			}
 		}
-		return (1f / 4f) * C(x0) * C(y0) * accu;
+		return 1f / 4f * C(x0) * C(y0) * accu;
 	}
 
 	public double DCT(int x0, int y0, byte[] matrix, int offset, int rows, int columns) {
-		if (max < Math.max(rows, columns)) {
+		if (this.max < Math.max(rows, columns)) {
 			throw new IndexOutOfBoundsException();
 		}
-		if ((x0 < 0) || (y0 < 0) || (max < x0) || (max < y0) || (rows <= 0) || (columns <= 0)) {
+		if (x0 < 0 || y0 < 0 || this.max < x0 || this.max < y0 || rows <= 0 || columns <= 0) {
 			throw new IndexOutOfBoundsException();
 		}
 
 		double accu = 0;
 		for (int y = 0; y < rows; ++y) {
 			for (int x = 0; x < columns; ++x) {
-				accu += cof(x, x0) * cof(y, y0) * matrix[offset + x + (y * columns)];
+				accu += cof(x, x0) * cof(y, y0) * matrix[offset + x + y * columns];
 			}
 		}
-		return (1f / 4f) * C(x0) * C(y0) * accu;
+		return 1f / 4f * C(x0) * C(y0) * accu;
 	}
 
 }
