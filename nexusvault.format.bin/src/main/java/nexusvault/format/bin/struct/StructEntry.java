9fe8efc8abd79a5ee153b8130394240ce7cec4ab
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2021-11-12 14:50:31 +0100

    format.tbl
    Fix version error
    Add ordering to structs

--- nexusvault.format.bin/src/main/java/nexusvault/format/bin/struct/StructEntry.java
+++ nexusvault.format.bin/src/main/java/nexusvault/format/bin/struct/StructEntry.java
@@ -2,22 +2,32 @@ package nexusvault.format.bin.struct;
 
 import static kreed.reflection.struct.DataType.BIT_32;
 
+import kreed.reflection.struct.Order;
 import kreed.reflection.struct.StructField;
 import kreed.reflection.struct.StructUtil;
+import nexusvault.shared.exception.StructException;
 
 public final class StructEntry {
 
+	static {
+		if (StructUtil.sizeOf(StructEntry.class) != 0x08) {
+			throw new StructException();
+		}
+	}
+
 	public static final int SIZE_IN_BYTES = StructUtil.sizeOf(StructEntry.class);
 
+	@Order(1)
 	@StructField(BIT_32)
-	public int id;
+	public int id; // 0x0
 
 	/**
 	 * Offset starts at {@link StructFileHeader#textOffset}. <br>
 	 * Each character is UTF16 encoded, hence, to compute the correct start position, this value needs to be multiplied with 2.
 	 */
+	@Order(2)
 	@StructField(BIT_32)
-	public int characterOffset;
+	public int characterOffset; // 0x4
 
 	public StructEntry() {
 
