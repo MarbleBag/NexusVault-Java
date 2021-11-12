3e580b941c5b4867ffb5f0ae03dd8f7374b62944
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2021-11-12 14:41:34 +0100

    format.tbl : Remove old classes

--- nexusvault.format.tbl/src/main/java/nexusvault/format/tbl/better/UntypedTable.java
+++
@@ -1,9 +0,0 @@
-package nexusvault.format.tbl.better;
-
-import java.util.List;
-
-public class UntypedTable extends XTable<Object[]> {
-	public UntypedTable(String name, Column[] columns, List<Object[]> entries, int[] lookup) {
-		super(name, columns, entries, lookup);
-	}
-}
